f530cd624cd9080b451627d2b673b43ff3523ddf
TinyProbe <137adv167@gmail.com>
2022-09-21 15:40:08 +0900

    update

--- client.h
+++
@@ -1,8 +0,0 @@
-#ifndef CLIENT_H
-# define CLIENT_H
-
-# include <unistd.h>
-# include <stdlib.h>
-# include <signal.h>
-
-#endif
--- server.h
+++
@@ -1,8 +0,0 @@
-#ifndef SERVER_H
-# define SERVER_H
-
-# include <unistd.h>
-# include <stdlib.h>
-# include <signal.h>
-
-#endif
